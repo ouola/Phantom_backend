0299eb42e8576d404d7ef14b6320beb569e32f54
OUOLA <d122973140@gmail.com>
2024-08-28 23:36:02 +0800

    implement Django basic setting and implement model.py for pharmacy and user data

---
+++ backend/phantom_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhantomAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'phantom_app'
---
+++ backend/phantom_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    cash_balance = models.DecimalField(max_digits=10, decimal_places=2)
+    opening_hours = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Mask(models.Model):
+    # 連接至Pharmacy
+    pharmacy = models.ForeignKey(Pharmacy, related_name='masks',on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f" {self.pharmacy.name} - {self.name}"
+    
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    cash_balance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class PurchaseHistory(models.Model):
+    user = models.ForeignKey(User, related_name='purchases', on_delete=models.CASCADE)
+    pharmacy = models.ForeignKey(Pharmacy, related_name='purchases', on_delete=models.CASCADE)
+    mask_name = models.CharField(max_length=255)
+    transaction_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.name} - {self.mask_name} - {self.transaction_amount} - {self.transaction_date}"
